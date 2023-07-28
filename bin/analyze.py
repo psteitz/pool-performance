498853dd8a493c912a03d981f5dfc678080ac1e3
Phil Steitz <psteitz@crapple.local>
2023-07-28 13:11:17 -0700

    Add no-latency configs, fix some bugs in analyze.

--- bin/analyze.py
+++ bin/analyze.py
@@ -1,68 +1,53 @@
 #!/usr/bin/env python3
 #
 # analyze.py
-# 0. Create an empty results array
-# 1. For each .tgz file in /results followed by each file the current directory,
-#   a. Unzip the .tgz file to overwrite reports in /results
-#   b. Add the data from each report to results[] an array of dictionaries,
+# 0. gzip the files in /results and move the .tgz to ~
+# 1. For each file in /results 
+#      Add the data from each report to results[] an array of dictionaries,
 #      each holding summary data from one report, identified by the
 #      pool version and config name.
-# 2. Write a summary report to /results/summary.csv
-
-# To process a single report in 2a, look at the file name, e.g,
-#     pool2-version-2.10.0-sample-GKOP.log
-# 
-# Each file creates a record with the following keys:
+# 2. For each .tgz in POOL_PERFORMACE_PATH
+#      Unzip the file, over-writing /results and repeat 1.
+# 3. Analyze the data in the aggregated results[] array and write 
+#    output csv to OUTPUT_PATH
+# 4. Copy gzip created in 0. back to HOME_PATH + POOL_PERFORMACE_PATH,
+#    and unzip it to restore /results
 #
-#   pool_version         : the version of the pool 2.10.0 for the example above)
-#   config_name          : the configuration of the pool (sample-GKOP for the example above)
+#   Records in results[] inclue
+#
+#   pool_version         : the version of the pool (e.g. 2.10.0, 2.12.0-SNAPSHOT)
+#   config_name          : the value of the <name> element in commons-performance xml file used iin the run
 #   latency_mean         : the mean latency across all client threads
 #   latency_stddev       : the standard deviation of the latency across all client threads
 #   on_time_startup_rate : the proportion of clients that started on time
 #   id                   : a unique long integer id for the record
 #   
-# Start by storing all records in an array of dictionaries, one for each report.
-
-# Loop over files in /results, reading each as a text file,
-# First set pool-version and config-name based on the file name.
-# Then look for the end block in the input file:
-#
-# Overall summary statistics (all threads combined) LATENCY
-# StatisticalSummaryValues:
-# n: 1000000
-# min: 0.0011900000972673297
-# max: 363.2711181640625
-# mean: 0.019214021268693867
-# std dev: 0.6825760452893023
-# 
-# Set latency_mean to the value of mean: in the block above.
-# Similarly, set latency_stddev to the value of std dev: in the block above.
-
-# To set on_time_startup_rate, search for the following block in the input file:
-#
-# Overall summary statistics (all threads combined) ON TIME STARTUP RATE
-# StatisticalSummaryValues:
-# n: 1000000
-# min: 0.0
-# max: 1.0
-# mean: 0.9998920000000002
-# std dev: 0.01039174884167453
-# 
-# Set on_time_startup_rate to the value of mean: in the block above.
-#
-# Finally, add a dictionary to the results array with the keys and values set above.\
-# 
-#
 
 import os
 import random
 
-results = []
-# loop over files in /results
-# Use absolute path to /results
 HOME_PATH = os.path.expanduser('~')
-RESULTS_PATH = HOME_PATH + "/commons-performance/src/pool/pool-performance/results"
-OUTPUT_PATH = HOME_PATH + "/commons-performance/src/pool/pool-performance/summary.csv"
+POOL_PERFORMACE_PATH = "/commons-performance/src/pool/pool-performance"
+RESULTS_PATH = HOME_PATH + POOL_PERFORMACE_PATH + "/results"
+OUTPUT_PATH = HOME_PATH + POOL_PERFORMACE_PATH + "/summary.csv"
+
+# Create orig.tgz containing the contents of RESULTS_PATH and move it to ~
+os.chdir(HOME_PATH + POOL_PERFORMACE_PATH)
+os.system("tar -czf orig.tgz .")
+os.system("mv orig.tgz ~")
+
+results = []
+
+# Build a string with the remaining tokens starting at the given index
+# in tokens, re-inserting "-" and removing the last 4 characters.
+# So for example remaining_minus_suffix(pool2-version-2.10.0-sample-GKOP.log)
+def remaining_minus_suffix(index, tokens):
+    out = ""
+    for i in range(index, len(tokens) - 1):
+        out = out + tokens[i] + "-"
+    out = out + tokens[len(tokens) - 1][:-4]
+    return out
+
 count = 0
 def process_files():
     global count
@@ -75,10 +60,10 @@ def process_files():
         #                0      1       2       3      4
         if "SNAPSHOT" in file:
             record["pool_version"] = name_tokens[2] + "-" + name_tokens[3]
-            record["config_name"] = name_tokens[4] + "-" + name_tokens[5][:-4]
+            record["config_name"] = remaining_minus_suffix(4, name_tokens)
         else:
             record["pool_version"] = name_tokens[2]
-            record["config_name"] = name_tokens[3] + "-" + name_tokens[4][:-4]
+            record["config_name"] = remaining_minus_suffix(3, name_tokens)
 
         # Set ID to the next sequential integer
         record["id"] = count;
@@ -121,14 +106,15 @@ process_files()
 #  b. Process the files in /results
 #
 # loop over files in HOME_PATH + "/commons-performance/src/pool/pool-performance/
-for file in os.listdir(HOME_PATH + "/commons-performance/src/pool/pool-performance/"):
+os.chdir(HOME_PATH + POOL_PERFORMACE_PATH)
+for file in os.listdir(HOME_PATH + POOL_PERFORMACE_PATH):
     # if the file name ends with .tgz
     if file.endswith(".tgz"):
         # unzip the file to overwrite reports in /results
-        os.system("tar -xzf " + file + " -C " + RESULTS_PATH)
+        os.system("tar -xzf " + file)
         # process the files in /results
         process_files()
-        print("Finished processing file " + file)
+        print("Finished processing tgz file " + file)
 
 # Display the count of files processed
 print("Processed " + str(count) + " files")
@@ -179,7 +165,6 @@ for version in versions:
         on_time_startup_rates = []
         for rec in results:
             if rec["pool_version"] == version and rec["config_name"] == config:
-                print("Found matching record " + str(rec) + " config " + config + " version " + version)
                 if "latency_mean" in rec and "latency_stddev" in rec and "on_time_startup_rate" in rec:
                     latency_means.append(rec["latency_mean"])
                     latency_stddevs.append(rec["latency_stddev"])
@@ -198,15 +183,23 @@ for version in versions:
             print("No good matching records for config " + config + " version " + version)
         print("Finished analyzing version " + version + " config " + config)
         print(str(record))
-    # write out the results
-    with open(OUTPUT_PATH, "w") as f2:
-        f2.write("pool_version,config_name,latency_mean,latency_stddev,on_time_startup_rate,num_reports\n")
-        for record in out:
-            out_str = record["pool_version"] + "," + record["config_name"] + \
-            "," + str(record["latency_mean"]) + "," + str(record["latency_stddev"]) + \
-            "," + str(record["on_time_startup_rate"]) + \
-            "," + str(record["num_reports"]) + "\n"
-            f2.write(out_str)
+
+# write out the results
+with open(OUTPUT_PATH, "w") as f2:
+    f2.write("pool_version,config_name,latency_mean,latency_stddev,on_time_startup_rate,num_reports\n")
+    for record in out:
+        out_str = record["pool_version"] + "," + record["config_name"] + \
+        "," + str(record["latency_mean"]) + "," + str(record["latency_stddev"]) + \
+        "," + str(record["on_time_startup_rate"]) + \
+        "," + str(record["num_reports"]) + "\n"
+        f2.write(out_str)
+
+# Restore state of /result to original
+os.chdir(HOME_PATH + POOL_PERFORMACE_PATH)
+os.system("mv ~/orig.tgz .")
+os.system("tar -xzf orig.tgz")
+os.system("rm orig.tgz")
+
 
 
 
